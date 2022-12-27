bdc101b5b538c4c83e2aec83f8664a58641f6370
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2022-12-27 14:38:23 +0100

    Add footer and update animation

--- js/hero.js
+++ js/hero.js
@@ -10,18 +10,24 @@ const position = main.offsetTop;
 
 window.addEventListener("scroll", () => {
     let percentage = 100 - (main.getBoundingClientRect().top - window.innerHeight) * 100 / (position - window.innerHeight);
-    if(percentage <= 110){
-        console.log(percentage)
+    if(percentage <= 100){
         welcome.style.backgroundSize = percentage + "%";
-        heroBannerImage.style.backgroundSize = (145 + (percentage * 0.3)) + "%";
+        heroBannerImage.style.backgroundSize = (100 + (percentage * 0.4)) + "%";
         sofianeLasoa.style.backgroundPosition = percentage + "%";
+    }else{
+        welcome.style.backgroundSize = 100 + "%";
+        heroBannerImage.style.backgroundSize = (100 + (100 * 0.4)) + "%";
+        sofianeLasoa.style.backgroundPosition = 100 + "%";
     }
 
     if(percentage > 100){
         heroBanner.style.position = "absolute";
         heroBanner.style.bottom = "0";
+        heroBannerImage.style.position = "absolute";
+        heroBannerImage.style.bottom = "0";
     }else{
         heroBanner.style.position = "fixed";
+        heroBannerImage.style.position = "fixed";
     }
 });
 
--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,65 @@
+/* Navigation animation*/
+
+/* Menu anim */
+
 const menuIcon = document.querySelector('.toogle-nav');
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('header nav');
 
 menuIcon.addEventListener('click', () => {
-    if(menuIcon.classList.contains('active')){
+    if(menu.classList.contains('active')){
         menuIcon.classList.remove('active');
         menu.classList.add('disactive');
         setTimeout(() => {
             menu.classList.remove('disactive');
             menu.classList.remove('active');
             nav.classList.remove('active');
-        }, 500)
-        
+        }, 500)   
     }else{
         menuIcon.classList.add('active');
         nav.classList.add('active');
-        setTimeout(() => {
-            menu.classList.add('active');
-        }, 1000)
+        menu.classList.add('active');
     }
 })
+
+/* Footer (animation when the user scroll in the footer) */
+
+const footer = document.querySelector('footer');
+const body = document.querySelector('body');
+
+window.addEventListener('scroll', () => {
+    let position = 0 - (footer.getBoundingClientRect().top - window.innerHeight);
+    let size = footer.getBoundingClientRect().height;
+    percentage = position * 70 / size;
+    if(percentage > 0){
+        body.style.padding = '0 ' + percentage + 'px';
+        body.style.boxShadow = '0 0 0 ' + (percentage + 0.2) + 'px #5154ad inset';
+    }else{
+        body.style.padding= 0 + 'px';
+        body.style.boxShadow = '0 0 0 ' + (0) + 'px #5154ad inset';
+    }
+})
+
+
+/* Footer (animation when mouse is over the contact block) */
+
+const contactBlock = document.querySelectorAll('.contact-block');
+const contactBlockH1 = document.querySelectorAll('.contact-block > h1');
+const contactBlockContent = document.querySelectorAll('.contact-block-content');
+
+for (let i = 0; i < contactBlock.length; i++) {
+    contactBlock[i].addEventListener('mouseover', () => {
+        if(!contactBlock[i].classList.contains('active')){
+            contactBlock[i].classList.add('active');
+            contactBlockH1[i].style.display = 'none';
+            contactBlockContent[i].classList.add('active');
+        }
+    })
+    contactBlock[i].addEventListener('mouseout', () => {
+        if(contactBlock[i].classList.contains('active')){
+            contactBlock[i].classList.remove('active');
+            contactBlockH1[i].style.display = 'block';
+            contactBlockContent[i].classList.remove('active');
+        }
+    }) 
+}
